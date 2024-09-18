089c1b00bba89a6e9b3ee9c60e92baa5ee7a30b3
Anish Mainali <anishmainali31@gmail.com>
2024-09-18 17:34:26 +0545

    routing and middlewares

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js"
+
 
 dotenv.config({});
 
@@ -22,6 +24,12 @@ const corsOptions ={
 app.use(cors(corsOptions)); 
 
 const PORT = process.env.PORT || 3000;
+
+//apis
+app.use("/api/v1/user", userRoute);
+
+
+
 
 app.listen(PORT, ()=>{ 
     connectDB();
